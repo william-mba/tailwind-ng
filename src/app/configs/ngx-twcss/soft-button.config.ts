6639a0e3f9fffaeae8ed361fdc9f5735ae91f348
William Mba <wmba@ecologiciel.com>
2024-05-31 18:16:25 -0400

    playground(buttons): add soft button custom config

---
+++ src/app/configs/ngx-twcss/soft-button.config.ts
@@ -0,0 +1,22 @@
+import { SoftButtonConfig } from "ngx-twcss";
+
+export const CustomSoftButtonConfig: SoftButtonConfig = {
+  theme: {
+    textColor: 'text-rose-600',
+    bgColor: 'bg-rose-600'
+  }
+}
+
+export const CustomSoftButtonConfig2: SoftButtonConfig = {
+  theme: {
+    textColor: 'text-orange-600',
+    bgColor: 'bg-orange-600'
+  }
+}
+
+export const CustomSoftButtonConfig3: SoftButtonConfig = {
+  theme: {
+    textColor: 'text-lime-600',
+    bgColor: 'bg-lime-600'
+  }
+}
