aedaf27fffd12da59d272a6e340816cf34e5667e
William Mba <wmba@ecologiciel.com>
2024-05-31 18:14:20 -0400

    playground(buttons): remove partials on custom config

--- src/app/configs/ngx-twcss/primary-button.config.ts
+++ src/app/configs/ngx-twcss/primary-button.config.ts
@@ -1,18 +1,18 @@
 import { PrimaryButtonConfig } from "ngx-twcss"
 
-export const CustomPrimaryButtonConfig: Partial<PrimaryButtonConfig> = {
+export const CustomPrimaryButtonConfig: PrimaryButtonConfig = {
   theme: {
     bgColor: 'bg-rose-500'
   }
 }
 
-export const CustomPrimaryButtonConfig2: Partial<PrimaryButtonConfig> = {
+export const CustomPrimaryButtonConfig2: PrimaryButtonConfig = {
   theme: {
     bgColor: 'bg-orange-600'
   }
 }
 
-export const CustomPrimaryButtonConfig3: Partial<PrimaryButtonConfig> = {
+export const CustomPrimaryButtonConfig3: PrimaryButtonConfig = {
   theme: {
     bgColor: 'bg-lime-600'
   }
