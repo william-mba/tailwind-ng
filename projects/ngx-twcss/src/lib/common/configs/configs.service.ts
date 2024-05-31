67781110cce543bddd80a2eca4d84f44dd60c0c8
William Mba <wmba@ecologiciel.com>
2024-05-31 11:37:33 -0400

    refactor(configs): refactor configs service as generic

--- projects/ngx-twcss/src/lib/common/configs/configs.service.ts
+++ projects/ngx-twcss/src/lib/common/configs/configs.service.ts
@@ -1,32 +1,25 @@
 import { BehaviorSubject } from 'rxjs';
 import { Injectable } from "@angular/core";
-import { PrimaryButtonConfig } from "../../components/buttons/primary-button/primary-button.config";
 import { mergeObjects } from "../helpers/object.helper";
-import { SecondaryButtonConfig } from '../../components/buttons/secondary-button/secondary-button.config';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ConfigsService {
+export class ConfigService<T> {
+  private configs: Record<string, BehaviorSubject<Partial<T>>> = {};
 
-  private _primaryButtonConfigSubject = new BehaviorSubject<Partial<PrimaryButtonConfig>>(PrimaryButtonConfig);
-  private _secondaryButtonConfigSubject = new BehaviorSubject<Partial<SecondaryButtonConfig>>(SecondaryButtonConfig);
-
-  public primaryButtonConfig() {
-    return this._primaryButtonConfigSubject.asObservable();
+  get(key: string) {
+    return this.configs[key].asObservable();
   }
 
-  public setPrimaryButtonConfig(config: Partial<PrimaryButtonConfig>): void {
-    const newConfig = mergeObjects(PrimaryButtonConfig, config);
-    this._primaryButtonConfigSubject.next(newConfig);
-  }
+  set(key: string, target: Partial<T>, source: Partial<T> = {}): ConfigService<T> {
 
-  public secondaryButtonConfig() {
-    return this._secondaryButtonConfigSubject.asObservable();
-  }
+    if (!this.configs[key]?.value) {
+      this.configs[key] = new BehaviorSubject<Partial<T>>({});
+    }
+    const config = mergeObjects(target, source);
+    this.configs[key].next(config);
 
-  public setSecondaryButtonConfig(config: Partial<SecondaryButtonConfig>): void {
-    const newConfig = mergeObjects(SecondaryButtonConfig, config);
-    this._secondaryButtonConfigSubject.next(newConfig);
+    return this;
   }
 }
