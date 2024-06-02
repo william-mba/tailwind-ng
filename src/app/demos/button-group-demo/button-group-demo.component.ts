61120901a557b76d47d49efc748830d51e73000a
William Mba <wmba@ecologiciel.com>
2024-06-02 16:42:48 -0400

    demos: add a view for button group demo

---
+++ src/app/demos/button-group-demo/button-group-demo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-group-demo',
+  templateUrl: './button-group-demo.component.html',
+})
+export class ButtonGroupDemoComponent {
+
+}
