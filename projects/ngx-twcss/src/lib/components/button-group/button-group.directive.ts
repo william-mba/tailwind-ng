4ef53ddd5e53e4f8fc5e3f3c514b9d26bff8ae15
William Mba <wmba@ecologiciel.com>
2024-06-13 22:09:30 -0400

    Components(button GROUP): add unit test

--- projects/ngx-twcss/src/lib/components/button-group/button-group.directive.ts
+++ projects/ngx-twcss/src/lib/components/button-group/button-group.directive.ts
@@ -1,14 +1,11 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive } from '@angular/core';
 
 /**Button group element*/
 @Directive({
   selector: 'nxt-button-group',
-  standalone: true
-})
-export class ButtonGroup {
-  @Input() className: string = 'inline-flex';
-
-  constructor(private el: ElementRef) {
-    this.el.nativeElement.className = this.className;
+  standalone: true,
+  host: {
+    class: 'inline-flex'
   }
-}
+})
+export class ButtonGroup { }
