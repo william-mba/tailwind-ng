abd04745a8ddc730e8ae5c6905a192f2173ff1fc
William Mba <wmba@ecologiciel.com>
2024-06-20 13:13:08 -0400

    components(badge): refatcor static style

--- projects/ngx-twcss/src/lib/components/badge/badge.component.ts
+++ projects/ngx-twcss/src/lib/components/badge/badge.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BaseConfig } from "../../configs/base.config";
 import { BaseComponent } from '../base.component';
+import { BackgroundOpacity } from '../../core/types/backgrounds/background-opacity';
+import { RingWidth } from '../../core/types/borders/ring-width';
+import { RingOpacity } from '../../core/types/borders/ring-opacity';
+import { FontSize } from '../../core/types/typography/font-size';
 
 /**Badge element config key
  * @package ngx-twcss
@@ -25,14 +29,20 @@ export const BadgeConfig: BadgeConfig = {
   templateUrl: './badge.component.html'
 })
 export class Badge extends BaseComponent<BadgeConfig> implements OnInit {
-  @Input() override style!: string[];
+  /* Used here instead of in the config to avoid
+  the value to be override when coming from the className.*/
+  private readonly padding: string = 'px-2 py-1';
+  private readonly fontSize: FontSize = 'text-xs';
+  private readonly border: RingWidth = 'ring-inset';
+  private readonly ringOpacity: RingOpacity = 'ring-opacity-30';
+  private readonly bgOpacity: BackgroundOpacity = 'bg-opacity-5';
+  private baseStyle: string = 'dark:bg-opacity-5 dark:ring-opacity-10';
+
   @Input() override className!: string;
 
   ngOnInit(): void {
-    const staticStyle = 'ring-inset ring-1 px-2 py-1 ' +
-      'text-xs bg-opacity-5 ring-opacity-30 ' +
-      'dark:bg-opacity-10 dark:ring-opacity-10 ';
-    this.className = staticStyle + this.className;
+    this.baseStyle += ` ${this.padding} ${this.fontSize} ${this.border} ${this.ringOpacity} ${this.bgOpacity}`;
+    this.className += ` ${this.baseStyle}`;
     this.initConfig(BadgeConfigKey, BadgeConfig);
   }
 }
