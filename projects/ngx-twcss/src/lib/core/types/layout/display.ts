c747988d38d318d6df4b407987979b628cc7a85d
William Mba <wmba@ecologiciel.com>
2024-07-01 15:38:31 -0400

    core: add new properties

--- projects/ngx-twcss/src/lib/core/types/layout/display.ts
+++ projects/ngx-twcss/src/lib/core/types/layout/display.ts
@@ -1,27 +1,72 @@
 import { AlignItem } from "../flex-grid/align-item"
+import { FlexDirection } from "../flex-grid/flex-direction"
+import { FlexGrow } from "../flex-grid/flex-grow"
+import { FlexShrink } from "../flex-grid/flex-shrink"
+import { FlexWrap } from "../flex-grid/flex-wrap"
 import { Gap } from "../flex-grid/gap"
+import { GridColumns } from "../flex-grid/grid-cols"
 import { JustifyContent } from "../flex-grid/justify-content"
+import { PlaceContent } from "../flex-grid/place-content"
+import { PlaceItems } from "../flex-grid/place-items"
 
 export type Display = Partial<{
   type:
   | "flex"
-  | "sm:flex"
-  | "*:sm:flex"
   | "grid"
+  | "sm:flex"
+  | "sm:grid"
   | "inline-flex"
   | "inline-grid",
-  /**Utilities for controlling gutters between grid and flexbox items.
-   * @see https://tailwindcss.com/docs/gap
-   */
-  gap: Gap,
+
   /**Utilities for controlling how flex and grid items are positioned along a container's cross axis.
    * @see https://tailwindcss.com/docs/align-items
    */
   alignItem: AlignItem,
+
+  /**Utilities for controlling the direction of flex items.
+   * @see https://tailwindcss.com/docs/flex-direction
+   */
+  flexDirection: FlexDirection,
+
+  /**Utilities for controlling how flex items grow.
+   * @see https://tailwindcss.com/docs/flex-grow
+   */
+  flexGrow: FlexGrow,
+
+  /**Utilities for controlling how flex items shrink.
+   * @see https://tailwindcss.com/docs/flex-shrink
+   */
+  flexShrink: FlexShrink,
+
+  /**Utilities for controlling how flex items wrap.
+   * @see https://tailwindcss.com/docs/flex-wrap
+   */
+  flexWrap: FlexWrap,
+
+  /**Utilities for controlling gutters between grid and flexbox items.
+   * @see https://tailwindcss.com/docs/gap
+   */
+  gap: Gap,
+
+  /**Utilities for specifying the columns in a grid layout.
+   * @see https://tailwindcss.com/docs/grid-template-columns
+   */
+  gridCols: GridColumns,
+
   /**Utilities for controlling how flex and grid items are positioned along a container's main axis.
    * @see https://tailwindcss.com/docs/justify-content
    */
-  justifyContent: JustifyContent
+  justifyContent: JustifyContent,
+
+  /**Utilities for controlling how content is justified and aligned at the same time.
+   * @see https://tailwindcss.com/docs/place-content
+   */
+  placeContent: PlaceContent,
+
+  /**Utilities for controlling how items are justified and aligned at the same time.
+   * @see https://tailwindcss.com/docs/place-items
+   */
+  placeItems: PlaceItems,
 }>
   | "block"
   | "table"
@@ -41,8 +86,9 @@ export type Display = Partial<{
   | "contents"
   | "list-item"
   | "flex"
-  | "sm:flex"
   | "grid"
+  | "sm:flex"
+  | "sm:grid"
   | "inline-flex"
   | "inline-grid"
 
