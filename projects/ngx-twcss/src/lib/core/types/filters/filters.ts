5110520ebdfc4318f9b9f31093562016f6373444
William Mba <wmba@ecologiciel.com>
2024-07-02 15:43:07 -0400

    core: add new types

---
+++ projects/ngx-twcss/src/lib/core/types/filters/filters.ts
@@ -0,0 +1,9 @@
+import { Backdrop } from "./backdrop"
+import { Blur } from "./blur"
+import { DropShadow } from "./drop-shadow"
+
+export type Filters = {
+  blur: Blur
+  backdrop: Backdrop
+  dropShadow: DropShadow
+}
