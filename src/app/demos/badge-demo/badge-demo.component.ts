30a9402d543d0bd231b37f72094654e2c7b4b0e7
William Mba <wmba@ecologiciel.com>
2024-07-11 01:35:19 -0400

    demos(badge): update

--- src/app/demos/badge-demo/badge-demo.component.ts
+++ src/app/demos/badge-demo/badge-demo.component.ts
@@ -7,29 +7,38 @@ import { Component } from '@angular/core';
 })
 export class BadgeDemoComponent {
 
-  flat = `<tw-badge className="bg-blue-600 text-blue-600 rounded-md">Badge</tw-badge>
-<tw-badge className="bg-red-600 text-red-500 rounded-md">Badge</tw-badge>
-<tw-badge className="bg-yellow-600 text-yellow-600 rounded-md">Badge</tw-badge>`;
+  flat = `<tw-badge className="text-gray-500 bg-gray-500/10 rounded-">Badge</tw-badge>
+<tw-badge className="text-blue-500 bg-blue-500/10 rounded-">Badge</tw-badge>
+<tw-badge className="text-red-500 bg-red-500/10 rounded-">Badge</tw-badge>
+<tw-badge className="text-yellow-500 bg-yellow-500/10 rounded-">Badge</tw-badge>`;
 
-  withBorder = `<tw-badge className="bg-blue-600 text-blue-600 ring-1 ring-blue-300 rounded-md">Badge</tw-badge>
-<tw-badge className="bg-red-600 text-red-500 ring-1 ring-red-300 rounded-md">Badge</tw-badge>
-<tw-badge className="bg-yellow-600 text-yellow-600 ring-1 ring-yellow-300 rounded-md">Badge</tw-badge>`;
+  withBorder = `<tw-badge className="text-gray-500 bg-gray-500/10 ring-gray-500/20 ring-1 rounded-">Badge</tw-badge>
+<tw-badge className="text-blue-500 bg-blue-500/10 ring-blue-500/20 ring-1 rounded-">Badge</tw-badge>
+<tw-badge className="text-red-500 bg-red-500/10 ring-red-500/20 ring-1 rounded-">Badge</tw-badge>
+<tw-badge className="text-yellow-500 bg-yellow-500/10 ring-yellow-500/20 ring-1 rounded-">Badge</tw-badge>`;
 
-  withLeadingIcon = `<tw-badge className="bg-blue-600 text-blue-600 rounded-md">
+  withLeadingIcon = `<tw-badge className="text-gray-500 bg-gray-500/10 rounded-">
   <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
     <path
       d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
   </svg>
   Badge
 </tw-badge>
-<tw-badge className="bg-red-600 text-red-500 rounded-md">
+<tw-badge className="text-blue-500 bg-blue-500/10 rounded-">
   <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
     <path
       d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
   </svg>
   Badge
 </tw-badge>
-<tw-badge className="bg-yellow-600 text-yellow-600 rounded-md">
+<tw-badge className="text-red-500 bg-red-500/10 rounded-">
+  <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+    <path
+      d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
+  </svg>
+  Badge
+</tw-badge>
+<tw-badge className="text-yellow-500 bg-yellow-500/10 rounded-">
   <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
     <path
       d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
@@ -37,101 +46,29 @@ export class BadgeDemoComponent {
   Badge
 </tw-badge>`;
 
-  withLeadingIconAndBorder = `<tw-badge className="bg-blue-600 text-blue-600 ring-1 ring-blue-300 rounded-md rounded-md">
+  withLeadingIconAndBorder = `<tw-badge
+  className="text-gray-500 bg-gray-500/10 ring-gray-500/20 ring-1 rounded-">
   <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
     <path
       d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
   </svg>
   Badge
 </tw-badge>
-<tw-badge className="bg-red-600 text-red-500 ring-1 ring-red-300 rounded-md rounded-md">
+<tw-badge className="text-blue-500 bg-blue-500/10 ring-blue-500/20 ring-1 rounded-">
   <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
     <path
       d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
   </svg>
   Badge
 </tw-badge>
-<tw-badge className="bg-yellow-600 text-yellow-600 ring-1 ring-yellow-300 rounded-md rounded-md">
-  <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
-    <path
-      d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
-  </svg>
-  Badge
-</tw-badge>`;
-
-  withActionButton = `<tw-badge className="bg-blue-600 text-blue-600 rounded-md">
-  Badge
-  <button tw-badge-action>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
-      <path d="m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z" />
-    </svg>
-  </button>
-</tw-badge>
-<tw-badge className="bg-red-600 text-red-500 rounded-md">
-  Badge
-  <button tw-badge-action>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
-      <path d="m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z" />
-    </svg>
-  </button>
-</tw-badge>
-<tw-badge className="bg-yellow-600 text-yellow-600 rounded-md">
-  Badge
-  <button tw-badge-action>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
-      <path d="m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z" />
-    </svg>
-  </button>
-</tw-badge>`;
-
-  withActionButtonAndBorder = `<tw-badge className="bg-blue-600 text-blue-600 ring-1 ring-blue-300 rounded-md">
-  Badge
-  <button tw-badge-action>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
-      <path d="m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z" />
-    </svg>
-  </button>
-</tw-badge>
-<tw-badge className="bg-red-600 text-red-500 ring-1 ring-red-300 rounded-md">
-  Badge
-  <button tw-badge-action>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
-      <path d="m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z" />
-    </svg>
-  </button>
-</tw-badge>
-<tw-badge className="bg-yellow-600 text-yellow-600 ring-1 ring-yellow-300 rounded-md">
-  Badge
-  <button tw-badge-action>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
-      <path d="m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z" />
-    </svg>
-  </button>
-</tw-badge>`;
-
-  roundedFlat = `<tw-badge className="bg-blue-600 text-blue-600">Badge</tw-badge>
-<tw-badge className="bg-red-600 text-red-500">Badge</tw-badge>
-<tw-badge className="bg-yellow-600 text-yellow-600">Badge</tw-badge>`;
-
-  roundedWithBorder = `<tw-badge className="bg-blue-600 text-blue-600 ring-1 ring-blue-300">Badge</tw-badge>
-<tw-badge className="bg-red-600 text-red-500 ring-1 ring-red-300">Badge</tw-badge>
-<tw-badge className="bg-yellow-600 text-yellow-600 ring-1 ring-yellow-300">Badge</tw-badge>`;
-
-  roundedWithLeadingIcon = `<tw-badge className="bg-blue-600 text-blue-600">
+<tw-badge className="text-red-500 bg-red-500/10 ring-red-500/20 ring-1 rounded-">
   <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
     <path
       d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
   </svg>
   Badge
 </tw-badge>
-<tw-badge className="bg-red-600 text-red-500">
-  <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
-    <path
-      d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
-  </svg>
-  Badge
-</tw-badge>
-<tw-badge className="bg-yellow-600 text-yellow-600">
+<tw-badge className="text-yellow-500 bg-yellow-500/10 ring-yellow-500/20 ring-1 rounded-">
   <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
     <path
       d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
@@ -139,29 +76,7 @@ export class BadgeDemoComponent {
   Badge
 </tw-badge>`;
 
-  roundedWithLeadingIconAndBorder = `<tw-badge className="bg-blue-600 text-blue-600 ring-1 ring-blue-300">
-  <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
-    <path
-      d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
-  </svg>
-  Badge
-</tw-badge>
-<tw-badge className="bg-red-600 text-red-500 ring-1 ring-red-300">
-  <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
-    <path
-      d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
-  </svg>
-  Badge
-</tw-badge>
-<tw-badge className="bg-yellow-600 text-yellow-600 ring-1 ring-yellow-300">
-  <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
-    <path
-      d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
-  </svg>
-  Badge
-</tw-badge>`;
-
-  roundedWithActionButton = `<tw-badge className="bg-blue-600 text-blue-600">
+  withActionButton = `<tw-badge className="text-gray-500 bg-gray-500/10 rounded-">
   Badge
   <button tw-badge-action>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
@@ -169,7 +84,7 @@ export class BadgeDemoComponent {
     </svg>
   </button>
 </tw-badge>
-<tw-badge className="bg-red-600 text-red-500">
+<tw-badge className="text-blue-500 bg-blue-500/10 rounded-">
   Badge
   <button tw-badge-action>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
@@ -177,7 +92,15 @@ export class BadgeDemoComponent {
     </svg>
   </button>
 </tw-badge>
-<tw-badge className="bg-yellow-600 text-yellow-600">
+<tw-badge className="text-red-500 bg-red-500/10 rounded-">
+  Badge
+  <button tw-badge-action>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+      <path d="m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z" />
+    </svg>
+  </button>
+</tw-badge>
+<tw-badge className="text-yellow-500 bg-yellow-500/10 rounded-">
   Badge
   <button tw-badge-action>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
@@ -186,7 +109,7 @@ export class BadgeDemoComponent {
   </button>
 </tw-badge>`;
 
-  roundedWithActionButtonAndBorder = `<tw-badge className="bg-blue-600 text-blue-600 ring-1 ring-blue-300">
+  withActionButtonAndBorder = `<tw-badge className="text-gray-500 bg-gray-500/10 ring-gray-500/20 ring-1 rounded-">
   Badge
   <button tw-badge-action>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
@@ -194,7 +117,7 @@ export class BadgeDemoComponent {
     </svg>
   </button>
 </tw-badge>
-<tw-badge className="bg-red-600 text-red-500 ring-1 ring-red-300">
+<tw-badge className="text-blue-500 bg-blue-500/10 ring-blue-500/20 ring-1 rounded-">
   Badge
   <button tw-badge-action>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
@@ -202,7 +125,141 @@ export class BadgeDemoComponent {
     </svg>
   </button>
 </tw-badge>
-<tw-badge className="bg-yellow-600 text-yellow-600 ring-1 ring-yellow-300">
+<tw-badge className="text-red-500 bg-red-500/10 ring-red-500/20 ring-1 rounded-">
+  Badge
+  <button tw-badge-action>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+      <path d="m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z" />
+    </svg>
+  </button>
+</tw-badge>
+<tw-badge className="text-yellow-500 bg-yellow-500/10 ring-yellow-500/20 ring-1 rounded-">
+  Badge
+  <button tw-badge-action>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+      <path d="m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z" />
+    </svg>
+  </button>
+</tw-badge>`;
+
+  roundedFlat = `<tw-badge className="text-gray-500 bg-gray-500/10">Badge</tw-badge>
+<tw-badge className="text-blue-500 bg-blue-500/10">Badge</tw-badge>
+<tw-badge className="text-red-500 bg-red-500/10">Badge</tw-badge>
+<tw-badge className="text-yellow-500 bg-yellow-500/10">Badge</tw-badge>`;
+
+  roundedWithBorder = `<tw-badge className="text-gray-500 bg-gray-500/10 ring-gray-500/20 ring-1">Badge</tw-badge>
+<tw-badge className="text-blue-500 bg-blue-500/10 ring-blue-500/20 ring-1">Badge</tw-badge>
+<tw-badge className="text-red-500 bg-red-500/10 ring-red-500/20 ring-1">Badge</tw-badge>
+<tw-badge className="text-yellow-500 bg-yellow-500/10 ring-yellow-500/20 ring-1">Badge</tw-badge>`;
+
+  roundedWithLeadingIcon = `<tw-badge className="text-gray-500 bg-gray-500/10">
+  <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+    <path d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
+  </svg>
+  Badge
+</tw-badge>
+<tw-badge className="text-blue-500 bg-blue-500/10">
+  <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+    <path d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
+  </svg>
+  Badge
+</tw-badge>
+<tw-badge className="text-red-500 bg-red-500/10">
+  <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+    <path d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
+  </svg>
+  Badge
+</tw-badge>
+<tw-badge className="text-yellow-500 bg-yellow-500/10">
+  <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+    <path d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
+  </svg>
+  Badge
+</tw-badge>`;
+
+  roundedWithLeadingIconAndBorder = `<tw-badge className="text-gray-500 bg-gray-500/10 ring-gray-500/20 ring-1">
+  <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+    <path d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
+  </svg>
+  Badge
+</tw-badge>
+<tw-badge className="text-blue-500 bg-blue-500/10 ring-blue-500/20 ring-1">
+  <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+    <path d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
+  </svg>
+  Badge
+</tw-badge>
+<tw-badge className="text-red-500 bg-red-500/10 ring-red-500/20 ring-1">
+  <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+    <path d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
+  </svg>
+  Badge
+</tw-badge>
+<tw-badge className="text-yellow-500 bg-yellow-500/10 ring-yellow-500/20 ring-1">
+  <svg class="size-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+    <path d="M480-200q-117 0-198.5-81.5T200-480q0-117 81.5-198.5T480-760q117 0 198.5 81.5T760-480q0 117-81.5 198.5T480-200Z" />
+  </svg>
+  Badge
+</tw-badge>`;
+
+  roundedWithActionButton = `<tw-badge className="text-gray-500 bg-gray-500/10">
+  Badge
+  <button tw-badge-action>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+      <path d="m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z" />
+    </svg>
+  </button>
+</tw-badge>
+<tw-badge className="text-blue-500 bg-blue-500/10">
+  Badge
+  <button tw-badge-action>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+      <path d="m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z" />
+    </svg>
+  </button>
+</tw-badge>
+<tw-badge className="text-red-500 bg-red-500/10">
+  Badge
+  <button tw-badge-action>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+      <path d="m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z" />
+    </svg>
+  </button>
+</tw-badge>
+<tw-badge className="text-yellow-500 bg-yellow-500/10">
+  Badge
+  <button tw-badge-action>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+      <path d="m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z" />
+    </svg>
+  </button>
+</tw-badge>`;
+
+  roundedWithActionButtonAndBorder = `<tw-badge className="text-gray-500 bg-gray-500/10 ring-gray-500/20 ring-1">
+  Badge
+  <button tw-badge-action>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+      <path d="m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z" />
+    </svg>
+  </button>
+</tw-badge>
+<tw-badge className="text-blue-500 bg-blue-500/10 ring-blue-500/20 ring-1">
+  Badge
+  <button tw-badge-action>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+      <path d="m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z" />
+    </svg>
+  </button>
+</tw-badge>
+<tw-badge className="text-red-500 bg-red-500/10 ring-red-500/20 ring-1">
+  Badge
+  <button tw-badge-action>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+      <path d="m291-240-51-51 189-189-189-189 51-51 189 189 189-189 51 51-189 189 189 189-51 51-189-189-189 189Z" />
+    </svg>
+  </button>
+</tw-badge>
+<tw-badge className="text-yellow-500 bg-yellow-500/10 ring-yellow-500/20 ring-1">
   Badge
   <button tw-badge-action>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
