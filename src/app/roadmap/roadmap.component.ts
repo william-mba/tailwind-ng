05478ec43ae1ce8026f7bbc027e7dacd50584cea
William Mba <wmba@ecologiciel.com>
2024-07-12 00:21:13 -0400

    add roadmap page

---
+++ src/app/roadmap/roadmap.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-roadmap',
+  templateUrl: './roadmap.component.html',
+  styles: ``
+})
+export class RoadmapComponent {
+
+}
