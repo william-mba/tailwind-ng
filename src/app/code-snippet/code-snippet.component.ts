9fb0ec3cb2a51116a55df0139238ce6ea144e987
William Mba <wmba@ecologiciel.com>
2024-07-15 07:58:48 -0400

    fix position

--- src/app/code-snippet/code-snippet.component.ts
+++ src/app/code-snippet/code-snippet.component.ts
@@ -10,7 +10,7 @@ import { asyncScheduler, concatMap, scheduled, timer } from 'rxjs';
       {{ label }}
     </summary>
     <div class="relative">
-      <tw-button size="sm" variant="text" className="absolute -right-1.5 -top-4 font-mono" (click)="copy()">
+      <tw-button size="sm" variant="text" className="absolute -right-2 -top-6 font-mono" (click)="copy()">
         {{ text }}
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
           <path d="M379.78-233.78q-44.3 0-75.15-30.85-30.85-30.85-30.85-75.15v-466.44q0-44.3 30.85-75.15 30.85-30.85 75.15-30.85h346.44q44.3 0 75.15 30.85 30.85 30.85 30.85 75.15v466.44q0 44.3-30.85 75.15-30.85 30.85-75.15 30.85H379.78Zm-186 186q-44.3 0-75.15-30.85-30.85-30.85-30.85-75.15v-572.44h106v572.44h452.44v106H193.78Z"/>
