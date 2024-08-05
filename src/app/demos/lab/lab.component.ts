76fff56d1c9ddbd4015d9f489b01059d12e4ed2f
William Mba <wmba@ecologiciel.com>
2024-08-05 03:22:57 -0400

    remove combobxe

--- src/app/demos/lab/lab.component.ts
+++ src/app/demos/lab/lab.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Comboboxe } from 'ngx-twcss';
 
 @Component({
   selector: 'demo-lab',
-  templateUrl: './lab.component.html',
-  styles: ``
+  templateUrl: './lab.component.html'
 })
 export class LabComponent {
 
