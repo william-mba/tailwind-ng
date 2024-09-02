5d49252f423fa09a1ebcf5f8d7e7962bc1a15d77
William Mba <wmba@ecologiciel.com>
2024-09-02 18:36:38 -0400

    test(forms): fix failling test

--- projects/ngx-twcss/src/lib/components/forms/combobox/combobox-item/combobox-item.component.spec.ts
+++ projects/ngx-twcss/src/lib/components/forms/combobox/combobox-item/combobox-item.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ComboboxItemComponent } from './combobox-item.component';
 import { provideComboboxItemConfig } from './combobox-item.config';
 import { ComboboxItem } from './combobox-item';
+import { ComboboxComponent } from '../combobox.component';
+import { provideComboboxConfig } from '../combobox.config';
 
 describe('Combobox Item Component', () => {
   let component: ComboboxItem;
@@ -10,11 +12,14 @@ describe('Combobox Item Component', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ComboboxItemComponent],
-      providers: [provideComboboxItemConfig()]
+      providers: [provideComboboxItemConfig(), provideComboboxConfig()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ComboboxItemComponent);
     component = fixture.componentInstance;
+    TestBed.runInInjectionContext(() => {
+      component.combobox = new ComboboxComponent();
+    });
     fixture.detectChanges();
   });
 
