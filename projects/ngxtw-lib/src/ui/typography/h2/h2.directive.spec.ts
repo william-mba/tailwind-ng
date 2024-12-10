6733ed5366e26ae528e021b27170ba74b87685f2
William Mba <wmba@ecologiciel.com>
2024-12-10 11:42:26 -0500

    lib(components): remove typography module

--- projects/ngxtw-lib/src/ui/typography/h2/h2.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { H2 } from './h2.directive';
-
-describe('H2Directive', () => {
-  it('should create an instance', () => {
-    const directive = new H2();
-    expect(directive).toBeTruthy();
-  });
-});
--- projects/ngxtw-lib/src/ui/typography/h4/h4.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { H4 } from './h4.directive';
-
-describe('H3Directive', () => {
-  it('should create an instance', () => {
-    const directive = new H4();
-    expect(directive).toBeTruthy();
-  });
-});
