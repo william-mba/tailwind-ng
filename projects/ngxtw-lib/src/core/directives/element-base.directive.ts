c717fa4519ae1714548b9a808cfee0579e0fc937
William Mba <wmba@ecologiciel.com>
2024-12-16 04:04:24 -0500

    refactor(core): use inputs and outputs properties in base element directive

--- projects/ngxtw-lib/src/core/directives/element-base.directive.ts
+++ projects/ngxtw-lib/src/core/directives/element-base.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, inject, input, model, OnInit } from "@angular/core";
+import { Directive, ElementRef, inject, OnInit, OutputEmitterRef } from "@angular/core";
 import { BaseElement } from "./element-base.interface";
 import { ClassList } from "../../config/classlist";
 import { ReactiveConfig } from "../../public-api";
@@ -8,23 +8,24 @@ import { stringToArray } from "../helpers/string.helper";
 @Directive({
   host: {
     '[class]': 'classList.value',
-    '[attr.disabled]': 'isDisabled() ? "" : null',
-    '[attr.aria-disabled]': 'isDisabled() ? "" : null',
-    '[attr.tabindex]': 'isDisabled() ? -1 : null',
-  }
+    '[attr.disabled]': 'isDisabled ? "" : null',
+    '[attr.aria-disabled]': 'isDisabled ? "" : null',
+    '[attr.tabindex]': 'isDisabled ? -1 : null',
+  },
+  inputs: [{ name: 'class', transform: stringToArray }, 'isDisabled'],
+  outputs: ['disabled', 'enabled'],
 })
 export abstract class ElementBaseDirective<T extends HTMLElement = HTMLElement> implements BaseElement<T>, OnInit {
   protected readonly _config = inject(ReactiveConfig)
   readonly nativeElement: T = inject(ElementRef).nativeElement;
-
-  /**
-   * The initial class list setted before the component initialization.
-   */
-  class = input([], { transform: stringToArray });
-  readonly classList = new ClassList(this.class());
-  isDisabled = model(false, { alias: 'disabled' });
+  readonly classList = new ClassList();
+  class: string[] = [];
+  isDisabled = false;
+  disabled = new OutputEmitterRef<void>();
+  enabled = new OutputEmitterRef<void>();
 
   ngOnInit(): void {
+    this.classList.init(this.class);
     this.onInit();
   }
 
@@ -34,14 +35,16 @@ export abstract class ElementBaseDirective<T extends HTMLElement = HTMLElement> 
   protected abstract onInit(): void;
 
   enable(): void {
-    this.isDisabled.set(false);
+    this.isDisabled = false;
     this.nativeElement.addEventListener('click', this.nativeElement.click);
     this.onInit();
+    this.enabled.emit();
   }
 
   disable(): void {
-    this.isDisabled.set(true);
+    this.isDisabled = true;
     this.nativeElement.removeEventListener('click', this.nativeElement.click);
     this.onInit();
+    this.disabled.emit();
   }
 }
--- projects/ngxtw-lib/src/core/directives/element-base.interface.ts
+++ projects/ngxtw-lib/src/core/directives/element-base.interface.ts
@@ -1,4 +1,4 @@
-import { InputSignal, InputSignalWithTransform, ModelSignal } from "@angular/core";
+import { OutputEmitterRef } from "@angular/core";
 import { ClassList } from "../../config/classlist";
 import { SizeOption } from "../types/size-options.type";
 
@@ -6,16 +6,19 @@ export interface BaseElement<T extends HTMLElement = HTMLElement> {
   /**
  * The initial class list setted before the component initialization.
  */
-  readonly class: InputSignalWithTransform<string[], unknown>;
+  readonly class: string[];
   /**
    * A reference to the class list instance of the component.
    */
   readonly classList: ClassList;
-  readonly isDisabled: ModelSignal<boolean>;
+  readonly isDisabled: boolean;
   readonly nativeElement: T;
-  readonly size?: InputSignal<SizeOption>
+  readonly size?: SizeOption
 
   disable(): void;
   enable(): void;
+
+  disabled: OutputEmitterRef<void>;
+  enabled: OutputEmitterRef<void>;
 }
 
