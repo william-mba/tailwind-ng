b19f2815d92c931e5c4f10f4576fc23c4a22692d
William Mba <wmba@ecologiciel.com>
2024-12-16 03:59:21 -0500

    refactor(core): add resolve options interface to resolve function params to improve maintainability

--- projects/ngxtw-lib/src/core/helpers/string.helper.ts
+++ projects/ngxtw-lib/src/core/helpers/string.helper.ts
@@ -9,19 +9,32 @@ export function merge(target: string[], source: string[], separator?: string): s
   return separator ? [...set].join(separator) : [...set];
 }
 
+export interface ResolveOptions {
+  /** Whether to keep the class-deletor in the target array. */
+  keepClassDeletor: boolean;
+  /** The minimum string length. Default is 2.
+   */
+  minStringLength: number;
+}
+
 /** Resolves strings from source to target.
  * - Replace target values that partially or fully match source value(s).
  * - Adds to target, source value(s) that does not end by the '-' character.
- * @param keepClassDeletor Whether to keep the class-deletor in the target array.
+ * @param resolveOpts The resolve options.
  * @returns An array of resolved strings. */
-export function resolve(target: string[], source: string[], keepClassDeletor = false): string[] {
+export function resolve(target: string[], source: string[], opts?: Partial<ResolveOptions>): string[] {
   if (!source || source.length === 0) return target;
   /* Given an array of strings to resolve from source to target
     For each value in source, remove target values that partially or fully match the source value.
     If the source value don't ends by the character '-' (class-deletor), add it to target.
   */
-  const temp: string[] = []
-  const minStringLength = 3 // The minimum class name length. ex: p-0 = 3
+  const temp: string[] = [];
+
+  opts = {
+    minStringLength: opts?.minStringLength || 2,
+    keepClassDeletor: opts?.keepClassDeletor || false
+  }
+
   for (let i = 0; i < source.length; i++) {
     /* If source value end with '-', remove all target values that partially or fully match.
       ex.:
@@ -30,16 +43,17 @@ export function resolve(target: string[], source: string[], keepClassDeletor = f
         result: ''
       Note: text-blue-500 will be remove in target
     */
-    if (source[i].length >= minStringLength) {
+    if (source[i].length >= opts.minStringLength!) {
       // Check if source value has a class-deletor '-'. ex: 'text-blue-'
       if (source[i].charAt(source[i].length - 1) === '-') {
-        const searchString = source[i].substring(0, source[i].length - 1);
+        const lengthsEqual = source[i].length === opts.minStringLength;
+        const searchString = source[i].substring(0, lengthsEqual ? source[i].length : source[i].length - 1);
         target = target.filter(value => {
           return !value.startsWith(searchString);
         })
         // Keep the class-deletor in the target array.
-        // Needed when setting the initial class list of a component.
-        if (keepClassDeletor) {
+        // Usefull when setting the initial class list of a component.
+        if (opts.keepClassDeletor) {
           temp.push(source[i]);
         }
       } else {
