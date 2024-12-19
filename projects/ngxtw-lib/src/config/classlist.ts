b34e19fd36a518632735cf969de0d34857a76c32
William Mba <wmba@ecologiciel.com>
2024-12-19 16:45:38 -0500

    refactor(lib/config): improve ClassList API

--- projects/ngxtw-lib/src/config/classlist.ts
+++ projects/ngxtw-lib/src/config/classlist.ts
@@ -1,16 +1,28 @@
-import { resolve } from "../core/helpers/string.helper";
+import { StringHelper } from "../core/helpers/string.helper";
 import { objectToArray } from "../core/helpers/object.helper";
 import { ConfigValue } from "../core/types/config.type";
+
+export interface ClassList {
+  init(value: string[]): void;
+  set(value: string[]): void;
+  update(value: string[]): void;
+  initFrom<T extends ConfigValue>(config: T): void;
+  setFrom<T extends ConfigValue>(config: T): void;
+  updateFrom<T extends ConfigValue>(config: T): void;
+}
 
 /**
  * Class list instance of the component.
  */
-export class ClassList {
-  private _value: string[] = [];
+export class ClassList implements ClassList {
+  private _value!: string[];
   /**
    * The current class list value.
    */
   get value(): string[] {
+    if (!this._value) {
+      this._value = this._base;
+    }
     return this._value;
   };
 
@@ -36,7 +48,7 @@ export class ClassList {
    * - Resolves class list from given `value` to `this.base` keeping class deletor.
    */
   init(value: string[]): void {
-    this._base = resolve(this._base, value, { keepClassDeletor: true });
+    this._base = StringHelper.resolve(this._base, value, { keepClassDeletor: true });
   }
 
   /**
@@ -44,7 +56,7 @@ export class ClassList {
    * - Resolves class list from `this.base` to given `value`.
    */
   set(value: string[]): void {
-    this._value = resolve(value, this._base);
+    this._value = StringHelper.resolve(value, this._base);
   }
 
   /**
@@ -52,7 +64,7 @@ export class ClassList {
    * - Resolves class list from given `value` to current `this.value`.
    */
   update(value: string[]): void {
-    this._value = resolve(this._value, value);
+    this._value = StringHelper.resolve(this._value, value);
   }
 
   /**
