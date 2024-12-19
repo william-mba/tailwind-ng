ef6278f320157edbb71345bcd65abed106e5f3cb
William Mba <wmba@ecologiciel.com>
2024-12-19 16:36:08 -0500

    test(lib/helpers): add more tests for StringHelper

--- projects/ngxtw-lib/src/core/helpers/tests/string.helper.spec.ts
+++ projects/ngxtw-lib/src/core/helpers/tests/string.helper.spec.ts
@@ -1,9 +1,10 @@
 import { StringHelper } from '../string.helper';
 
 describe('StringHelper', () => {
-  it('should resolve strings', () => {
-    const source = ['bg-blue-600', 'text-blue-600', 'ring-blue-600', 'ring-2', 'translate-x-0', 'scale-100'];
-    const target = ['bg-red-100', 'text-red-600', 'ring-red-600', 'text-sm', 'shadow-md', 'ring-inset', 'translate-0', 'scale-y-100', 'translate-y-0'];
+  fit('should resolve values', () => {
+    const source = ['bg-blue-600', 'text-blue-600', 'ring-blue-600', 'ring-2', 'translate-x-0', 'scale-100', 'p-3.5'];
+    const target = ['bg-red-100', 'text-red-600', 'text-sm', 'ring-red-600', 'translate-0', 'translate-y-0',
+      'ring-inherit', 'ring-inset', 'rounded-md', 'scale-y-100', 'px-3.5', 'py-2'];
 
     const result = StringHelper.resolve(target, source);
 
@@ -11,15 +12,18 @@ describe('StringHelper', () => {
       expect(result.includes(value)).toBeTrue();
     });
 
-    expect(result.includes(target[0])).toBeFalse();
-    expect(result.includes(target[1])).toBeFalse();
-    expect(result.includes(target[2])).toBeFalse();
-    expect(result.includes(target[3])).toBeTrue();
-    expect(result.includes(target[4])).toBeTrue();
-    expect(result.includes(target[5])).toBeTrue();
-    expect(result.includes(target[6])).toBeTrue();
-    expect(result.includes(target[7])).toBeTrue();
-    expect(result.includes(target[8])).toBeTrue();
+    expect(result.includes('bg-red-100')).toBeFalse();
+    expect(result.includes('text-red-600')).toBeFalse();
+    expect(result.includes('text-sm')).toBeTrue();
+    expect(result.includes('rounded-md')).toBeTrue();
+    expect(result.includes('ring-red-600')).toBeFalse();
+    expect(result.includes('ring-inherit')).toBeFalse();
+    expect(result.includes('ring-inset')).toBeTrue();
+    expect(result.includes('translate-0')).toBeTrue();
+    expect(result.includes('translate-y-0')).toBeFalse();
+    expect(result.includes('scale-y-100')).toBeFalse();
+    expect(result.includes('px-3.5')).toBeFalse();
+    expect(result.includes('py-2')).toBeFalse();
   });
 
   it('should convert string to array', () => {
