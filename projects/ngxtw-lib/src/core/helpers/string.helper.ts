c5400d848ea0cef34b38866d8e3bfb35a3f4ebfb
William Mba <wmba@ecologiciel.com>
2024-12-19 16:32:51 -0500

    (WIP/DEBUG): add tests for StringHelper

--- projects/ngxtw-lib/src/core/helpers/string.helper.ts
+++ projects/ngxtw-lib/src/core/helpers/string.helper.ts
@@ -1,15 +1,6 @@
 import { isString } from "./type-check.helper";
 
-/** Merges strings from source to target.
- * @param separator The character to use as separator in the set of merged strings.
- * @returns An array of merged string set or a string set separated by the given separator. */
-export function merge(target: string[], source: string[], separator?: string): string[] | string {
-  if (!source || source.length === 0) return separator ? target.join(separator) : target;
-  let set = new Set<string>([...target, ...source]);
-  return separator ? [...set].join(separator) : [...set];
-}
-
-export interface ResolveOptions {
+interface ResolveOptions {
   /** Whether to keep the class-deletor in the target array. */
   keepClassDeletor: boolean;
   /** The minimum string length. Default is 2.
@@ -22,8 +13,11 @@ export interface ResolveOptions {
  * - Adds to target, source value(s) that does not end by the '-' character.
  * @param resolveOpts The resolve options.
  * @returns An array of resolved strings. */
-export function resolve(target: string[], source: string[], opts?: Partial<ResolveOptions>): string[] {
+function resolve(target: string[], source: string[], opts?: Partial<ResolveOptions>): string[] {
   if (!source || source.length === 0) return target;
+  if (!target || target.length === 0) return source;
+  if ((!source || source.length === 0) && (!target || target.length === 0)) return [];
+
   /* Given an array of strings to resolve from source to target
     For each value in source, remove target values that partially or fully match the source value.
     If the source value don't ends by the character '-' (class-deletor), add it to target.
@@ -36,15 +30,12 @@ export function resolve(target: string[], source: string[], opts?: Partial<Resol
   }
 
   for (let i = 0; i < source.length; i++) {
-    /* If source value end with '-', remove all target values that partially or fully match.
-      ex.:
-        target: 'text-blue-500'
-        source: 'text-blue-'
-        result: ''
-      Note: text-blue-500 will be remove in target
-    */
     if (source[i].length >= opts.minStringLength!) {
-      // Check if source value has a class-deletor '-'. ex: 'text-blue-'
+      // When the class name is a class-deletor,
+      // all target values starting with the class name should be removed.
+      // ex:
+      //  - 'bg-red-' remove 'bg-red-*',
+      //  - 'bg-' remove 'bg-*' etc.
       if (source[i].charAt(source[i].length - 1) === '-') {
         const lengthsEqual = source[i].length === opts.minStringLength;
         const searchString = source[i].substring(0, lengthsEqual ? source[i].length : source[i].length - 1);
@@ -57,10 +48,111 @@ export function resolve(target: string[], source: string[], opts?: Partial<Resol
           temp.push(source[i]);
         }
       } else {
-        const separatorIndex = source[i].lastIndexOf('-');
-        const searchString = separatorIndex > 0 ? source[i].substring(0, separatorIndex) : source[i];
+        // When the class name is not a class-deletor
+        let lastIndexOfSeperator = source[i].lastIndexOf('-');
+        let searchString = lastIndexOfSeperator > 0 ? source[i].substring(0, lastIndexOfSeperator) : source[i];
+        const foundInSource = Array.from(source[i].matchAll(/-/g)).length;
+
         target = target.filter(value => {
-          return !value.startsWith(searchString);
+          const foundInTarget = Array.from(value.matchAll(/-/g)).length;
+
+          // When target is for instance 'bg-blue-*' and source 'bg-red-*'
+          if (foundInSource >= foundInTarget && foundInSource > 1) {
+            // When source is for instance 'text-blue-600' and target is 'text-sm'
+            if (foundInSource > foundInTarget && foundInTarget === 1) {
+              // if (value === 'rounded-md') {
+              //   console.group('if (foundInSource > foundInTarget && foundInTarget === 1)');
+              //   console.log(`source: ${source[i].length}`, source[i]);
+              //   console.log(`value: ${value.length}`, value);
+              //   console.log('keepped');
+              //   console.groupEnd();
+              // }
+              return true;
+            }
+
+            // When target is for instance 'bg-blue-*' and source is 'bg-blue-*'
+            if (value.startsWith(searchString)) return false;
+
+            // Else, we need to extract another string segment
+            // searchString 'bg-red' is truncate to 'bg'
+            lastIndexOfSeperator = searchString.lastIndexOf('-');
+            if (lastIndexOfSeperator > 0) {
+              searchString = searchString.substring(0, lastIndexOfSeperator);
+            }
+            const res = value.startsWith(searchString)
+            // if (value === 'rounded-md') {
+            //   console.group('after if (lastIndexOfSeperator > 0)');
+            //   console.log(`source: ${source[i].length}`, source[i]);
+            //   console.log(`value: ${value.length}`, value);
+            //   console.log('keepped', !res);
+            //   console.groupEnd();
+            // }
+            return !res;
+          } else if (foundInSource === foundInTarget) {
+            const matchPercentage = (value.length * 100) / source[i].length;
+            if (matchPercentage < 150) {
+              const res = value.startsWith(searchString)
+              // if (value === 'rounded-md') {
+              //   console.group('if (matchPercentage < 150)');
+              //   console.log(`source: ${source[i].length}`, source[i]);
+              //   console.log(`value: ${value.length}`, value);
+              //   console.log(`matchPercentage: ${matchPercentage}`);
+              //   console.log('keepped', !res);
+              //   console.groupEnd();
+              // }
+              return !res;
+            } else {
+              const res = value.startsWith(searchString)
+              // if (value === 'rounded-md') {
+              //   console.group('(else) of if (matchPercentage < 150)');
+              //   console.log(`source: ${source[i].length}`, source[i]);
+              //   console.log(`value: ${value.length}`, value);
+              // }
+
+              // When target is 'ring-2' and source is 'ring-inset' or
+              // target is `rounded-md` and source is `ring-2` for instance
+              if (matchPercentage < 170) {
+                // console.log(`if (matchPercentage < 170)`);
+                // console.log(`matchPercentage: ${matchPercentage}`);
+                // console.log('keepped', true);
+                // console.groupEnd();
+                return true;
+              }
+              // if (value === 'rounded-md') {
+              //   console.log(`matchPercentage: ${matchPercentage}`);
+              //   console.log('keepped', !res);
+              //   console.groupEnd();
+              // }
+              return !res;
+            }
+          } else if (foundInSource < foundInTarget) {
+            // When target value is for instance scale-y-100 and source value is scale-100
+            const matchPercentage = (value.length * 100) / source[i].length;
+            if (matchPercentage < 150) {
+              const res = value.startsWith(searchString)
+              // if (value === 'rounded-md') {
+              //   console.group('if (matchPercentage < 150) IN else if (foundInSource < foundInTarget)');
+              //   console.log(`source: ${source[i].length}`, source[i]);
+              //   console.log(`value: ${value.length}`, value);
+              //   console.log(`matchPercentage: ${matchPercentage}`);
+              //   console.log('keepped', !res);
+              //   console.groupEnd();
+              // }
+              return !res;
+            } else {
+              const res = value.startsWith(searchString)
+              // if (value === 'rounded-md') {
+              //   console.group('(else) IN else if (foundInSource < foundInTarget)');
+              //   console.log(`source: ${source[i].length}`, source[i]);
+              //   console.log(`value: ${value.length}`, value);
+              //   console.log(`matchPercentage: ${matchPercentage}`);
+              //   console.log('keepped', !res);
+              //   console.groupEnd();
+              // }
+              return !res;
+            }
+          }
+          return true;
         })
         temp.push(source[i]);
       }
@@ -72,4 +164,9 @@ export function resolve(target: string[], source: string[], opts?: Partial<Resol
 /** Transfroms string value to an array then returns it. Returns an empty array if value is undefined. */
 export function stringToArray(value: unknown): string[] {
   return isString(value) ? (value as string).split(' ') : [];
+}
+
+export abstract class StringHelper {
+  static resolve = resolve;
+  static toArray = stringToArray;
 }
