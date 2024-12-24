97b11fe283ac41d8d3796c962de2ea4c9cc54282
William Mba <wmba@ecologiciel.com>
2024-12-24 06:34:00 -0500

    test(multiple): add more tests for reliability

--- projects/ngxtw-lib/src/ui/forms/input/input.component.spec.ts
+++ projects/ngxtw-lib/src/ui/forms/input/input.component.spec.ts
@@ -1,25 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { InputComponent } from './input.component';
-import { provideInputConfig } from './input.config';
+import { InputConfig } from './input.config';
+import { ClassList } from '../../../config/classlist';
 
 describe('InputComponent', () => {
   let component: InputComponent;
   let fixture: ComponentFixture<InputComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [InputComponent],
-      providers: [provideInputConfig()]
-    })
-      .compileComponents();
-
     fixture = TestBed.createComponent(InputComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+
+  it('should get config', () => {
+    expect(component.config.get('Input').value).toEqual(InputConfig());
+  });
+
+  it('should set classlist', () => {
+    expect(component.classList.value).toEqual(new ClassList().setFrom(InputConfig()).value);
   });
 });
--- projects/ngxtw-lib/src/ui/forms/toggle/toggle.component.spec.ts
+++ projects/ngxtw-lib/src/ui/forms/toggle/toggle.component.spec.ts
@@ -1,27 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ToggleComponent } from './toggle.component';
-import { provideToggleConfig } from './toggle.config';
-import { Toggle } from './toggle.interface';
+import { ToggleConfig } from './toggle.config';
+import { ClassList } from '../../../config/classlist';
 
 describe('ToggleComponent', () => {
-  let component: Toggle;
-  let fixture: ComponentFixture<Toggle>;
+  let component: ToggleComponent;
+  let fixture: ComponentFixture<ToggleComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ToggleComponent],
-      providers: [
-        provideToggleConfig(),
-      ],
-    }).compileComponents();
-
     fixture = TestBed.createComponent(ToggleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should set isChecked', () => {
+    component.isChecked = true;
+    expect(component.isChecked).toBeTrue();
+  });
+
+  it('should toggle', () => {
+    expect(component.isChecked).toBeFalse();
+    component.toggle();
+    expect(component.isChecked).toBeTrue();
+  });
+
+  it('should get config', () => {
+    expect(component.config.get('Toggle').value).toEqual(ToggleConfig());
+  });
+
+  it('should set classlist', () => {
+    expect(component.classList.value).toEqual(new ClassList().setFrom(ToggleConfig()).value);
   });
 });
