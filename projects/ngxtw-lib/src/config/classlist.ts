e34cfbd7857f1d86d8a863bd0567a673c0b95a91
William Mba <wmba@ecologiciel.com>
2025-01-07 00:23:24 -0500

    feat(config): add a method to easily convert classlist to string

--- projects/ngxtw-lib/src/config/classlist.ts
+++ projects/ngxtw-lib/src/config/classlist.ts
@@ -2,18 +2,46 @@ import { StringHelper } from "../core/helpers/string.helper";
 import { ObjectHelper } from "../core/helpers/object.helper";
 import { ConfigValue } from "../core/types/config.type";
 
+/**
+ * @ngxtw An interface to manipulate the component's class list.
+ */
 export interface ClassList {
-  init(value: string | string[]): ClassList;
+  /**
+   * Initializes the class list.
+   * - Resolves class list from given `value` to `this.base` keeping class deletor.
+   */
+  init(value?: string | string[]): ClassList;
+  /**
+   * Sets a brand new class list.
+   * - Resolves class list from `this.base` to given `value`.
+   */
   set(value: string | string[]): ClassList;
+  /**
+   * Updates the current class list.
+   * - Resolves class list from given `value` to current `this.value`.
+   */
   update(value: string | string[]): ClassList;
+  /**
+   * Initializes class list using the specified config object.
+   * - Calls `this.init` method passing the given `config` converted to `string[]`.
+   */
   initFrom<T extends ConfigValue>(config: T): ClassList;
+  /**
+   * Sets class list using config object.
+   * - Calls `this.set` method passing the given `config` converted to `string[]`.
+   */
   setFrom<T extends ConfigValue>(config: T): ClassList;
+  /**
+   * Updates class list using config object.
+   * - Calls `this.update` method passing the given `config` converted to `string[]`.
+   */
   updateFrom<T extends ConfigValue>(config: T): ClassList;
+  /**
+   * Returns the string representation of the class list value.
+   */
+  toString(): string;
 }
 
-/**
- * Class list instance of the component.
- */
 export class ClassList implements ClassList {
   private _value!: string[];
   /**
@@ -43,54 +71,38 @@ export class ClassList implements ClassList {
     this._base = base;
   }
 
-  /**
-   * Initializes the class list.
-   * - Resolves class list from given `value` to `this.base` keeping class deletor.
-   */
-  init(value: string | string[]): ClassList {
-    this._base = StringHelper.resolve(this._base, value, { keepClassDeletor: true });
+  init(value?: string | string[]): ClassList {
+    if (value && value.length > 0) {
+      this._base = StringHelper.resolve(this._base, value, { keepClassDeletor: true });
+    } else {
+      this._base = [];
+    }
     return this;
   }
 
-  /**
-   * Sets a brand new class list.
-   * - Resolves class list from `this.base` to given `value`.
-   */
   set(value: string | string[]): ClassList {
     this._value = StringHelper.resolve(value, this._base);
     return this;
   }
 
-  /**
-   * Updates the current class list.
-   * - Resolves class list from given `value` to current `this.value`.
-   */
   update(value: string | string[]): ClassList {
     this._value = StringHelper.resolve(this._value, value);
     return this;
   }
 
-  /**
-   * Initializes class list using the specified config object.
-   * - Calls `this.init` method passing the given `config` converted to `string[]`.
-   */
   initFrom<T extends ConfigValue>(config: T): ClassList {
     return this.init(ObjectHelper.toArray(config));
   }
 
-  /**
-   * Sets class list using config object.
-   * - Calls `this.set` method passing the given `config` converted to `string[]`.
-   */
   setFrom<T extends ConfigValue>(config: T): ClassList {
     return this.set(ObjectHelper.toArray(config));
   }
 
-  /**
-   * Updates class list using config object.
-   * - Calls `this.update` method passing the given `config` converted to `string[]`.
-   */
   updateFrom<T extends ConfigValue>(config: T) {
     return this.update(ObjectHelper.toArray(config));
+  }
+
+  toString(): string {
+    return this.value.join(' ');
   }
 }
