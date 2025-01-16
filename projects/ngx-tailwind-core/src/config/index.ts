8a95594ecc608b0e7c7ca861c0afe2e76875ebab
William Mba <wmba@ecologiciel.com>
2025-01-15 23:47:30 -0500

    core(config): update imports

--- projects/ngx-tailwind-core/src/config/index.ts
+++ projects/ngx-tailwind-core/src/config/index.ts
@@ -1,4 +1,6 @@
+export * from './interfaces';
 export * from './classlist';
+export * from './config-from.type';
 export * from './config-key';
 export * from './config-store';
 export * from './config.helper';
