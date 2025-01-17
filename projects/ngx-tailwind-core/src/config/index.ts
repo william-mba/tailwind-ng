4ad430c4cd2811713ec07d8b83ca5cdba3896eef
William Mba <wmba@ecologiciel.com>
2025-01-17 03:02:25 -0500

    feat(ngx-tailwind/core): improve the reactive config api by adding a get method that returns config corresponding to the given key.

--- projects/ngx-tailwind-core/src/config/index.ts
+++ projects/ngx-tailwind-core/src/config/index.ts
@@ -1,6 +1,6 @@
 export * from './interfaces';
 export * from './classlist';
-export * from './config-from.type';
+export * from './config-type-of.type';
 export * from './config-key.type';
 export * from './config-store';
 export * from './config.helper';
--- projects/ngx-tailwind-core/src/config/reactive-config.ts
+++ projects/ngx-tailwind-core/src/config/reactive-config.ts
@@ -3,51 +3,54 @@ import { BehaviorSubject } from "rxjs";
 import { ConfigKey } from "./config-key.type";
 import { ConfigStore } from "./config-store";
 import { mergeConfig } from "./config.helper";
-import { ConfigFrom } from "./config-from.type";
+import { ConfigTypeOf } from "./config-type-of.type";
 
 /**
  * @ngx-tailwind Reactive configuration interface.
  */
 export interface IReactiveConfig {
-  /** Returns a Subject of the configuration corresponding to the key.
-   * If it does not exist, it will be initialized with the default configuration
-   * @param key The configuration key
+  /** Returns an observable configuration corresponding to the given key. If not setted,
+   * it will be initialized with the default value provided by the configuration store which is always an empty object.
    */
-  get(key: ConfigKey): BehaviorSubject<ConfigFrom<typeof key>>;
+  get<K extends ConfigKey>(key: K): BehaviorSubject<ConfigTypeOf<K>>;
 
-  /** Updates the configuration value
-   * @param key The configuration key
-   * @param value The value to update
+  /** Sets a brand new configuration.
+   * Returns the `ReactiveConfig` instance after setting.
    */
-  update(key: ConfigKey, ...value: Partial<ConfigFrom<typeof key>>[]): void;
+  set<K extends ConfigKey>(key: K, value: ConfigTypeOf<K>): IReactiveConfig;
+
+  /** Updates the configuration.
+   * Returns the `ReactiveConfig` instance after updating.
+   */
+  update<K extends ConfigKey>(key: K, ...value: Partial<ConfigTypeOf<K>>[]): IReactiveConfig;
 }
 
 /** @ngx-tailwind Reactive configuration */
 @Injectable({ providedIn: 'root' })
 export class ReactiveConfig implements IReactiveConfig {
-  private readonly store = new Map<ConfigKey, BehaviorSubject<ConfigFrom<ConfigKey>>>();
-  protected readonly config: ConfigStore = inject(ConfigStore);
+  private readonly _configMap = new Map<ConfigKey, BehaviorSubject<ConfigTypeOf<ConfigKey>>>();
+  protected readonly store = inject(ConfigStore);
 
-  private set(key: ConfigKey, config: ConfigFrom<typeof key>): void {
-    if (!this.store.has(key)) {
-      this.store.set(key, new BehaviorSubject(config));
-    }
-  }
-
-  update(key: ConfigKey, ...value: Partial<ConfigFrom<typeof key>>[]): IReactiveConfig {
-    if (this.store.has(key)) {
-      const config = this.get(key)!;
-      config.next(mergeConfig([config.value!, ...value]));
+  update<K extends ConfigKey>(key: K, ...value: Partial<ConfigTypeOf<K>>[]): ReactiveConfig {
+    if (this._configMap.has(key)) {
+      const config = this.get(key);
+      config.next(mergeConfig([config.value, ...value]) as ConfigTypeOf<K>);
     }
     return this;
   }
 
-  get<T extends ConfigKey = ConfigKey>(key: T) {
+  get<K extends ConfigKey>(key: K): BehaviorSubject<ConfigTypeOf<K>> {
     // If there is no key in the store,
     // we set an initial value then return it
-    if (!this.store.has(key)) {
-      this.set(key, this.config[key as keyof ConfigStore]);
+    if (!this._configMap.has(key)) {
+      this.set(key, this.store[key as keyof ConfigStore] as ConfigTypeOf<K>);
     }
-    return this.store.get(key) as BehaviorSubject<ConfigFrom<typeof key>>;
+    return this._configMap.get(key) as BehaviorSubject<ConfigTypeOf<K>>;
+  }
+
+  set<K extends ConfigKey>(key: K, value: ConfigTypeOf<K>): ReactiveConfig {
+    const configSubject = new BehaviorSubject(value);
+    this._configMap.set(key, configSubject as BehaviorSubject<ConfigTypeOf<ConfigKey>>);
+    return this;
   }
 }
