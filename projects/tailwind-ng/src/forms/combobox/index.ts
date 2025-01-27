7ddadfc96b656aba6cc9f9c04465c13d5a7d42b2
William Mba <wmba@ecologiciel.com>
2025-01-27 01:08:30 -0500

    fix: remove an incorrect import that was blocking lib build

--- projects/tailwind-ng/src/forms/combobox/index.ts
+++ projects/tailwind-ng/src/forms/combobox/index.ts
@@ -1,6 +1,4 @@
-export * from '../../../../tailwind-ng-core/src/interfaces/combobox-item';
 export * from './combobox-item/combobox-item.component';
 export * from './combobox-item/combobox-item.component.config';
-export * from '../../../../tailwind-ng-core/src/interfaces/combobox';
 export * from './combobox.component';
 export * from './combobox.module';
