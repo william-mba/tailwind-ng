93771fdd5463fa363f6be1b3db2117e9b3bcf6bc
William Mba <wmba@ecologiciel.com>
2025-01-27 23:09:15 -0500

    docs: add a page for labs

--- projects/tailwind-ng-docs/src/app/app.routes.ts
+++ projects/tailwind-ng-docs/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'lab',
+    loadComponent: () => import('./components/lab/lab.component').then(m => m.LabComponent)
+  },
+  {
+    path: '',
+    loadComponent: () => import('./main/main.component').then(m => m.MainComponent)
+  }
+];
