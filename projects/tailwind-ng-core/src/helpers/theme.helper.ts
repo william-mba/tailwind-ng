5b50a64352cfb9126ac38f5172c50fbfba613c69
William Mba <wmba@ecologiciel.com>
2025-01-28 16:30:54 -0500

    feat(core): add a helper class for dark theme toggle

--- projects/tailwind-ng-core/src/helpers/theme.helper.ts
+++ projects/tailwind-ng-core/src/helpers/theme.helper.ts
@@ -4,3 +4,10 @@
 export function toggleTheme(token = "dark") {
   document.documentElement.classList.toggle(token)
 }
+
+export abstract class ThemeHelper {
+  static toggle = toggleTheme;
+  static get isDark() {
+    return document.documentElement.classList.contains('dark');
+  }
+}
