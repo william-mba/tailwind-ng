fa1f02aec73de8b5fbe66fa5577b068a295a3ab3
William Mba <wmba@ecologiciel.com>
2025-01-31 19:26:05 -0500

    fix(docs): remove circular deps

--- projects/tailwind-ng-docs/src/app/docs/docs.component.ts
+++ projects/tailwind-ng-docs/src/app/docs/docs.component.ts
@@ -3,7 +3,6 @@ import { ButtonComponent, DropdownComponent, IconDirective, BadgeComponent } fro
 import { OptionDirective, ThemeHelper } from '@tailwind-ng/core';
 import { NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { NAV_ITEMS } from '../app.routes';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -36,3 +35,113 @@ export class DocsComponent {
   }
   protected readonly navItems = NAV_ITEMS;
 }
+
+export interface NavItem {
+  label: string;
+  route: string;
+  description?: string;
+  category?: NavCategory;
+  children?: NavItem[];
+}
+
+type NavCategory = 'documentation' | 'components' | 'getting-started';
+
+export const NAV_ITEMS: NavItem[] = [
+  {
+    label: 'Documentation',
+    route: '/docs',
+    category: 'documentation',
+    children: [
+      {
+        label: 'Getting Started',
+        route: 'getting-started'
+      },
+      {
+        label: 'Components',
+        route: 'components'
+      },
+      {
+        label: 'Customization',
+        route: 'customization'
+      },
+      {
+        label: 'Roadmap',
+        route: 'roadmap'
+      }
+    ]
+  },
+  {
+    label: 'Getting Started',
+    route: '/getting-started',
+    category: 'getting-started',
+    children: [
+      {
+        label: 'Installation',
+        route: 'installation'
+      },
+      {
+        label: 'Customization',
+        route: 'customization'
+      },
+      {
+        label: 'Roadmap',
+        route: 'roadmap'
+      }
+    ]
+  },
+  {
+    label: 'Components',
+    route: '/components',
+    category: 'components',
+    children: [
+      {
+        label: 'Avatar',
+        route: 'avatars'
+      },
+      {
+        label: 'Badge',
+        route: 'badges'
+      },
+      {
+        label: 'Button',
+        route: 'buttons'
+      },
+      {
+        label: 'Button Group',
+        route: 'button-groups'
+      },
+      {
+        label: 'Checkbox',
+        route: 'checkboxes'
+      },
+      {
+        label: 'Combobox',
+        route: 'comboboxes'
+      },
+      {
+        label: 'Dialog',
+        route: 'dialogs'
+      },
+      {
+        label: 'Dropdown',
+        route: 'dropdowns'
+      },
+      {
+        label: 'Icon',
+        route: 'icons'
+      },
+      {
+        label: 'Input Group',
+        route: 'input-groups'
+      },
+      {
+        label: 'Radio Group',
+        route: 'radio-groups'
+      },
+      {
+        label: 'Toggle',
+        route: 'toggles'
+      }
+    ]
+  }
+];
