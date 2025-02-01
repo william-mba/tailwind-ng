6aca3b492419e6a5432c1eb6f507c460557cacfa
William Mba <wmba@ecologiciel.com>
2025-01-31 19:37:44 -0500

    feat(core/helpers): add method in ThemeHelper for better dark/light mode management

--- projects/tailwind-ng-core/src/helpers/theme.helper.ts
+++ projects/tailwind-ng-core/src/helpers/theme.helper.ts
@@ -1,12 +1,27 @@
-/**Toggle theme
- * @param token The class name to toggle in the class list of the root node of the document. Default is 'dark'
-*/
-export function toggleTheme(token = "dark") {
-  document.documentElement.classList.toggle(token)
-}
-
 export abstract class ThemeHelper {
-  static toggle = toggleTheme;
+  /**
+   * Toggle the theme between light and dark.
+   */
+  static toggle() {
+    document.documentElement.classList.toggle('dark');
+    localStorage.setItem('theme', this.isDark ? 'dark' : 'default');
+  }
+  /**
+   * Initialize the theme based on the user's previous theme preference.
+   *
+   * If there is no previous theme preference, the default theme will be used.
+   * @param theme The theme to initialize. If setted, this will override previous user's theme preference.
+   */
+  static init(theme: 'default' | 'dark' = 'default') {
+    if (theme === 'dark' && !this.isDark) {
+      this.toggle();
+    } else if (localStorage.getItem('theme') === 'dark' && !this.isDark) {
+      this.toggle();
+    }
+  }
+  /**
+   * Returns true if the current theme is dark. Otherwise, returns false.
+   */
   static get isDark() {
     return document.documentElement.classList.contains('dark');
   }
