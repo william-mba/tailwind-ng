d01e4b7a1ab9353a5919a220d8787547977569a8
William Mba <wmba@ecologiciel.com>
2025-01-31 19:18:02 -0500

    docs: update app config to add some icons and routes

--- projects/tailwind-ng-docs/src/app/app.routes.ts
+++ projects/tailwind-ng-docs/src/app/app.routes.ts
@@ -46,3 +46,113 @@ export const routes: Routes = [
     loadComponent: () => import('./main/main.component').then(m => m.MainComponent)
   }
 ];
+
+export interface NavItem {
+  label: string;
+  route: string;
+  description?: string;
+  category?: NavCategory;
+  children?: NavItem[];
+}
+
+type NavCategory = 'documentation' | 'components' | 'getting-started';
+
+export const NAV_ITEMS: NavItem[] = [
+  {
+    label: 'Documentation',
+    route: '/docs',
+    category: 'documentation',
+    children: [
+      {
+        label: 'Getting Started',
+        route: 'getting-started'
+      },
+      {
+        label: 'Components',
+        route: 'components'
+      },
+      {
+        label: 'Customization',
+        route: 'customization'
+      },
+      {
+        label: 'Roadmap',
+        route: 'roadmap'
+      }
+    ]
+  },
+  {
+    label: 'Getting Started',
+    route: '/getting-started',
+    category: 'getting-started',
+    children: [
+      {
+        label: 'Installation',
+        route: 'installation'
+      },
+      {
+        label: 'Customization',
+        route: 'customization'
+      },
+      {
+        label: 'Roadmap',
+        route: 'roadmap'
+      }
+    ]
+  },
+  {
+    label: 'Components',
+    route: '/components',
+    category: 'components',
+    children: [
+      {
+        label: 'Avatar',
+        route: 'avatars'
+      },
+      {
+        label: 'Badge',
+        route: 'badges'
+      },
+      {
+        label: 'Button',
+        route: 'buttons'
+      },
+      {
+        label: 'Button Group',
+        route: 'button-groups'
+      },
+      {
+        label: 'Checkbox',
+        route: 'checkboxes'
+      },
+      {
+        label: 'Combobox',
+        route: 'comboboxes'
+      },
+      {
+        label: 'Dialog',
+        route: 'dialogs'
+      },
+      {
+        label: 'Dropdown',
+        route: 'dropdowns'
+      },
+      {
+        label: 'Icon',
+        route: 'icons'
+      },
+      {
+        label: 'Input Group',
+        route: 'input-groups'
+      },
+      {
+        label: 'Radio Group',
+        route: 'radio-groups'
+      },
+      {
+        label: 'Toggle',
+        route: 'toggles'
+      }
+    ]
+  }
+];
