dd32c7ff985270bc8f1cbc35de657d424b7ca976
William Mba <wmba@ecologiciel.com>
2025-02-01 22:41:34 -0500

    docs: scaffold roadmap doc

---
+++ projects/tailwind-ng-docs/src/app/docs/roadmap/roadmap.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roadmap',
+  imports: [],
+  templateUrl: './roadmap.component.html',
+  styles: ``
+})
+export class RoadmapComponent {
+
+}
