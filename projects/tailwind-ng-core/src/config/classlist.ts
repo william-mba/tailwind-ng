1d7dbdffd976113697b8874fd1d1286d92e0b280
William Mba <wmba@ecologiciel.com>
2025-02-04 16:23:42 -0500

    perf(core/config): use signals for better changes tracking

--- projects/tailwind-ng-core/src/config/classlist.ts
+++ projects/tailwind-ng-core/src/config/classlist.ts
@@ -1,3 +1,4 @@
+import { signal, Signal } from "@angular/core";
 import { Str, Obj, Type } from "../helpers";
 import { Config } from "../types";
 
@@ -5,25 +6,26 @@ import { Config } from "../types";
  * @TailwindNG Component's class list interface.
  */
 export interface IClassList {
-  readonly value: string[];
+  readonly base: Signal<string[]>;
+  readonly value: Signal<string[]>;
   /**
    * Initializes the class list value.
    */
-  init<T extends string>(value?: T): IClassList;
-  init<T extends string[]>(value?: T[]): IClassList;
-  init<T extends Config>(value?: T): IClassList;
+  init<T extends string>(value?: T): Promise<IClassList>;
+  init<T extends string[]>(value?: T[]): Promise<IClassList>;
+  init<T extends Config>(value?: T): Promise<IClassList>;
   /**
    * Sets a brand new class list value.
    */
-  set<T extends string>(value: T): IClassList;
-  set<T extends string[]>(value: T[]): IClassList;
-  set<T extends Config>(value: T): IClassList;
+  set<T extends string>(value: T): Promise<IClassList>;
+  set<T extends string[]>(value: T[]): Promise<IClassList>;
+  set<T extends Config>(value: T): Promise<IClassList>;
   /**
    * Updates the current class list value.
    */
-  update<T extends string>(value: T): IClassList;
-  update<T extends string[]>(value: T[]): IClassList;
-  update<T extends Config>(value: T): IClassList;
+  update<T extends string>(value: T): Promise<IClassList>;
+  update<T extends string[]>(value: T[]): Promise<IClassList>;
+  update<T extends Config>(value: T): Promise<IClassList>;
   /**
    * Returns the string representation of the class list value.
    */
@@ -35,100 +37,78 @@ export interface IClassList {
    * - 'value' clears the class list value only and keeps the base.
    * @returns The class list instance.
    */
-  clear(behavior: ClearBehavior): IClassList;
+  clear(behavior: ClearBehavior): Promise<IClassList>;
 }
 type ClearBehavior = 'all' | 'value';
 
 export class ClassList implements IClassList {
-  private _value!: string[];
   /**
    * The current class list value.
    */
-  get value(): string[] {
-    if (!this._value) {
-      this._value = this._base;
-    }
-    return this._value;
-  };
-
-  private _base: string[] = []
+  value = signal<string[]>([])
 
   /**
   * The custom class list to merge with the default class list's value.
   */
-  get base(): string[] {
-    return this._base;
-  }
+  base = signal<string[]>([]);
 
   /**
    * Creates a new instance of the class list.
    * @param base The custom class list to merge with the default class list's value.
    */
-  constructor(base: string[] = []) {
-    this._base = base;
+  constructor(base: string | string[] | Config = []) {
+    this.base.set(this.valueFrom(base));
   }
 
-  init<T extends string>(value?: T): ClassList;
-  init<T extends string[]>(value?: T[]): ClassList;
-  init<T extends Config>(value?: T): ClassList;
-
-  init<T extends string | string[] | Config>(value: T): ClassList {
-    if (!value) {
-      this._base = [];
-    } else {
-      if (Type.isString(value)) {
-        value = Str.toArray(value) as T;
-      } else if (Type.isArray(value)) {
-        value = value as T;
-      } else if (Type.isObject(value)) {
-        value = Obj.toArray(value as Config) as T;
-      }
-      this._base = Str.resolve([this._base, value as string[]], { keepClassDeletor: true });
-    }
+  async init<T extends string>(value?: T): Promise<ClassList>;
+  async init<T extends string[]>(value?: T[]): Promise<ClassList>;
+  async init<T extends Config>(value?: T): Promise<ClassList>;
+  async init<T extends string | string[] | Config>(value: T): Promise<ClassList> {
+    Str.resolve([this.base(), this.valueFrom(value)], { keepClassDeletor: true })
+      .then((result) => this.base.set(result));
     return this;
   }
 
-  set<T extends string>(value: T): ClassList;
-  set<T extends string[]>(value: T[]): ClassList;
-  set<T extends Config>(value: T): ClassList;
-
-  set<T extends string | string[] | Config>(value: T): ClassList {
-    if (Type.isString(value)) {
-      value = Str.toArray(value) as T;
-    } else if (Type.isArray(value)) {
-      value = value as T;
-    } else if (Type.isObject(value)) {
-      value = Obj.toArray(value as Config) as T;
-    }
-    this._value = Str.resolve([value as string[], this._base]);
+  async set<T extends string>(value: T): Promise<ClassList>;
+  async set<T extends string[]>(value: T[]): Promise<ClassList>;
+  async set<T extends Config>(value: T): Promise<ClassList>;
+  async set<T extends string | string[] | Config>(value: T): Promise<ClassList> {
+    Str.resolve([this.valueFrom(value), this.base()])
+      .then((result) => this.value.set(result));
     return this;
   }
 
-  update<T extends string>(value: T): ClassList;
-  update<T extends string[]>(value: T[]): ClassList;
-  update<T extends Config>(value: T): ClassList;
-
-  update<T extends string | string[] | Config>(value: T): ClassList {
-    if (Type.isString(value)) {
-      value = Str.toArray(value) as T;
-    } else if (Type.isArray(value)) {
-      value = value as T;
-    } else if (Type.isObject(value)) {
-      value = Obj.toArray(value as Config) as T;
-    }
-    this._value = Str.resolve([this._value, value as string[]]);
+  async update<T extends string>(value: T): Promise<ClassList>;
+  async update<T extends string[]>(value: T[]): Promise<ClassList>;
+  async update<T extends Config>(value: T): Promise<ClassList>;
+  async update<T extends string | string[] | Config>(value: T): Promise<ClassList> {
+    Str.resolve([this.value(), this.valueFrom(value)])
+      .then((result) => this.value.set(result));
     return this;
   }
 
   toString(): string {
-    return this.value.join(' ');
+    return this.value().join(' ');
   }
 
-  clear(behavior: ClearBehavior = 'all'): ClassList {
+  async clear(behavior: ClearBehavior = 'all'): Promise<ClassList> {
     if (behavior === 'all') {
-      this._base = [];
+      this.base.set([]);
     }
-    this._value = [];
+    this.value.set([]);
     return this;
   }
+
+  private valueFrom<T extends string | string[] | Config>(value: T): string[] {
+    let newValue: string[] = [];
+    if (Type.isString(value)) {
+      newValue = Str.toArray(value);
+    } else if (Type.isArray(value)) {
+      newValue = value;
+    } else if (Type.isObject(value)) {
+      newValue = Obj.toArray(value as Config) as string[];
+    }
+    return newValue;
+  }
+
 }
