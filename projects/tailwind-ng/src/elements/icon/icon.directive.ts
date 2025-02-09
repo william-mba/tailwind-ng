359ec216b78e3e1543c287be95e5e39a4c838912
William Mba <wmba@ecologiciel.com>
2025-02-09 04:38:14 -0500

    perf(component/icon): optimize icon rendering by removing blocking that may impact the main thread

--- projects/tailwind-ng/src/elements/icon/icon.directive.ts
+++ projects/tailwind-ng/src/elements/icon/icon.directive.ts
@@ -1,37 +1,27 @@
-import { Directive, Input } from '@angular/core';
+import { computed, Directive, inject, Input } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { SizeOption, IconBase, IconName, Icon, ClassList } from '@tailwind-ng/core';
 
 @Directive({
   selector: 'tw-icon, [twIcon], [tw-icon]',
   exportAs: 'twIcon',
-  host: { '[class]': 'classList.value()' },
+  host: {
+    '[class]': 'classList.value()',
+    '[innerHTML]': 'icon()'
+  },
   providers: [{ provide: IconBase, useExisting: IconDirective }]
 })
 export class IconDirective extends IconBase implements Icon {
+  private _sanitizer = inject(DomSanitizer);
   @Input() size: SizeOption = 'md';
   @Input({ required: true }) name!: IconName;
+  icon = computed(() => this._sanitizer.bypassSecurityTrustHtml(this.config.map![this.name] || ''));
 
   protected override async onInit(): Promise<void> {
     if (!this.classList) {
       this.classList = new ClassList(this.class);
       this.classList.init(this.config[this.size])
-        .then(() => {
-          this.classList.set(this.config);
-        })
-        .then(() => {
-          if (this.config.map) {
-            if (!this.config.map[this.name]) {
-              throw new Error(`Icon map does not contain the icon name: ${this.name}`);
-            } else {
-              this.nativeElement.innerHTML = this.config.map[this.name];
-            }
-          } else {
-            throw new Error('Icon map must be setted.');
-          }
-        })
-        .catch((reason) => {
-          console.error(reason);
-        });
+        .then(() => this.classList.set(this.config));
     }
   }
 }
