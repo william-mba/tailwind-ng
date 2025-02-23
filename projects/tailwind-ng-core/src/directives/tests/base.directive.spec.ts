46c4173266273af5ad05f1cd5d64086ec66cfa9b
William Mba <wmba@ecologiciel.com>
2025-02-23 03:36:40 -0500

    tests: add base directives tests cases

---
+++ projects/tailwind-ng-core/src/directives/tests/base.directive.spec.ts
@@ -0,0 +1,106 @@
+import { By } from '@angular/platform-browser';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { Component, Directive, ElementRef } from "@angular/core";
+import { BaseDirective } from "../base.directive";
+import { classlist } from '../../config/classlist';
+
+@Directive({
+  selector: '[fakeDirective]',
+}) class FakeDirective extends BaseDirective {
+  private _isFocused = false;
+  override get isFocused(): boolean {
+    return this._isFocused;
+  }
+  constructor() {
+    super();
+    this.nativeElement.focus = () => this._isFocused = true;
+  }
+  protected override buildStyle(): void {
+    this.classList = classlist(this.class);
+  }
+}
+
+describe('BaseDirective', () => {
+  let directive: FakeDirective;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        FakeDirective,
+        {
+          provide: ElementRef,
+          useValue: { nativeElement: document.createElement('div') }
+        }
+      ]
+    });
+    directive = TestBed.inject(FakeDirective);
+  });
+  it('should create', () => {
+    expect(directive).toBeTruthy();
+  })
+
+  describe('props', () => {
+    it('should set class', () => {
+      expect(directive.class()).toBeNull();
+
+      @Component({
+        selector: 'test-component',
+        imports: [FakeDirective],
+        template: `<div fakeDirective class="class1 class2 class3"></div>`
+      }) class TestComponent { }
+
+      const fixture = TestBed.createComponent(TestComponent);
+      directive = fixture.debugElement.query(By.directive(FakeDirective)).injector.get(FakeDirective);
+      fixture.detectChanges();
+
+      expect(directive.class()).toBe('class1 class2 class3');
+    });
+
+    it('should set classlist', () => {
+      expect(directive.classList).toBeUndefined();
+      directive.classList = classlist('class1 class2 class3');
+      expect(directive.classList.value()).toEqual(['class1', 'class2', 'class3']);
+    });
+
+    it('should get nativeElement', () => {
+      expect(directive.nativeElement).toBeTruthy();
+    });
+
+    it('should set disableable', () => {
+      expect(directive.disabled).toBeFalsy();
+      directive.disabled = true;
+      expect(directive.disabled).toBeTruthy();
+    });
+
+  })
+
+  describe('actions', () => {
+    it('should focus', () => {
+      expect(directive.isFocused).toBeFalsy();
+      directive.nativeElement.focus();
+      expect(directive.isFocused).toBeTruthy();
+    });
+
+    it('should add/remove visual focus', () => {
+      expect(directive.hasVisualFocus).toBeFalsy();
+      directive.setVisualfocus();
+      expect(directive.hasVisualFocus).toBeTruthy();
+      directive.removeVisualfocus();
+      expect(directive.hasVisualFocus).toBeFalsy();
+    });
+
+    it('should set whether it is hovered', () => {
+      expect(directive.isHovered).toBeFalsy();
+      directive.isHovered = true;
+      expect(directive.isHovered).toBeTruthy();
+    });
+
+    it('should scroll into view', fakeAsync(() => {
+      spyOn(directive.nativeElement, 'scrollIntoView');
+      directive.scrollIntoView();
+      tick(100);
+      expect(directive.nativeElement.scrollIntoView).toHaveBeenCalled();
+      expect(directive.nativeElement.scrollIntoView).toHaveBeenCalledTimes(1);
+      expect(directive.nativeElement.scrollIntoView).toHaveBeenCalledWith({ block: 'nearest', inline: 'nearest' });
+    }, { flush: true }));
+  })
+});
---
+++ projects/tailwind-ng-core/src/directives/tests/popup.directive.spec.ts
@@ -0,0 +1,79 @@
+import { computed, Directive, ElementRef } from "@angular/core";
+import { classlist } from '../../config/classlist';
+import { PopupDirective } from '../popup.directive';
+import { fakeAsync, TestBed, tick } from "@angular/core/testing";
+
+@Directive({
+  selector: '[fakePopup]',
+}) class FakePopup extends PopupDirective {
+  protected override buildStyle(): void {
+    this.classList = classlist(this.class);
+  }
+}
+
+describe('PopupDirective', () => {
+  let directive: FakePopup;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        FakePopup,
+        {
+          provide: ElementRef,
+          useValue: { nativeElement: document.createElement('div') }
+        }
+      ]
+    });
+    directive = TestBed.inject(FakePopup);
+  });
+  it('should create', () => {
+    expect(directive).toBeTruthy();
+  })
+
+  describe('props', () => {
+    it('should set id', () => {
+      expect(directive.id.startsWith('popup')).toBeTrue();
+    });
+  });
+
+  describe('actions', () => {
+    it('should toggle', fakeAsync(() => {
+      expect(directive.opened()).toBeFalse();
+      directive.toggle();
+      tick(100);
+      expect(directive.opened()).toBeTrue();
+      directive.toggle();
+      tick(100);
+      expect(directive.opened()).toBeFalse();
+    }, { flush: true }));
+
+    it('should open', fakeAsync(() => {
+      expect(directive.opened()).toBeFalse();
+      directive.open();
+      tick(100);
+      expect(directive.opened()).toBeTrue();
+    }, { flush: true }));
+
+    it('should close', fakeAsync(() => {
+      expect(directive.opened()).toBeFalse();
+      directive.open();
+      tick(100);
+      expect(directive.opened()).toBeTrue();
+      directive.close();
+      tick(100);
+      expect(directive.opened()).toBeFalse();
+    }, { flush: true }));
+  });
+
+  describe('advanced actions', () => {
+    it('should close after a given delay', fakeAsync(() => {
+      const opened = computed(() => directive.opened());
+      expect(opened()).toBeFalse();
+      directive.open();
+      tick(100);
+      expect(directive.opened()).toBeTrue();
+      directive.closeAfter(1000);
+      tick(2000);
+      expect(opened()).toBeFalse();
+    }, { flush: true }));
+  });
+});
