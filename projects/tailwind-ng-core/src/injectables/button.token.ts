bebbaa2490d256ba94a4165a604962b4c71a4a05
William Mba <wmba@ecologiciel.com>
2025-02-28 01:41:05 -0500

    fix(core): remove idle callback

--- projects/tailwind-ng-core/src/injectables/button.token.ts
+++ projects/tailwind-ng-core/src/injectables/button.token.ts
@@ -18,17 +18,15 @@ export abstract class ButtonBase extends BaseDirective<HTMLButtonElement> implem
   override ngOnInit(): void {
     super.ngOnInit();
     if (this.popup) {
-      requestIdleCallback(() => {
-        if (isDropdown(this.popup)) {
-          this.nativeElement.setAttribute('aria-haspopup', 'menu');
-        } else if (isDialog(this.popup)) {
-          this.nativeElement.setAttribute('aria-haspopup', 'dialog');
-        } else if (isCombobox(this.popup)) {
-          this.nativeElement.setAttribute('aria-haspopup', 'listbox');
-        } else {
-          this.nativeElement.setAttribute('aria-haspopup', 'true');
-        }
-      });
+      if (isDropdown(this.popup)) {
+        this.nativeElement.setAttribute('aria-haspopup', 'menu');
+      } else if (isDialog(this.popup)) {
+        this.nativeElement.setAttribute('aria-haspopup', 'dialog');
+      } else if (isCombobox(this.popup)) {
+        this.nativeElement.setAttribute('aria-haspopup', 'listbox');
+      } else {
+        this.nativeElement.setAttribute('aria-haspopup', 'true');
+      }
     }
   }
 }
