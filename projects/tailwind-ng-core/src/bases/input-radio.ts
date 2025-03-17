182e3b2f9cbb6e040a13b8a80e2b86642e358bf1
William Mba <wmba@ecologiciel.com>
2025-03-17 05:22:19 -0400

    refactor(components/forms): override input radio base to disable keyboard events prevention

--- projects/tailwind-ng-core/src/bases/input-radio.ts
+++ projects/tailwind-ng-core/src/bases/input-radio.ts
@@ -5,4 +5,12 @@ import { InjectionTokenFactory } from "../tokens/injection-token.factory";
 export const INPUT_RADIO_CONFIG = InjectionTokenFactory.create<string>('', 'INPUT_RADIO_CONFIG');
 
 @Directive()
-export abstract class InputRadioBase extends BaseDirective<HTMLInputElement> { }
+export abstract class InputRadioBase extends BaseDirective<HTMLInputElement> {
+  constructor() {
+    super()
+    // remove default keyboard event prevention.
+    // As necessary to navigate through radio inputs.
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    this.onKeyboardEvent = () => { };
+  }
+}
