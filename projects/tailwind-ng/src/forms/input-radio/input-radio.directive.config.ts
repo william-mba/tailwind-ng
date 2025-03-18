a1dbd9bed6a0c081c71002a436ce48b1de18a6aa
William Mba <wmba@ecologiciel.com>
2025-03-17 20:20:02 -0400

    feat(components/forms): add focus visible style on input radio

--- projects/tailwind-ng/src/forms/input-radio/input-radio.directive.config.ts
+++ projects/tailwind-ng/src/forms/input-radio/input-radio.directive.config.ts
@@ -2,7 +2,7 @@ import { Provider } from '@angular/core';
 import { INPUT_RADIO_CONFIG, ClassName } from "@tailwind-ng/core";
 
 const DefaultConfig = () => {
-  const className = 'appearance-none border-1 border-gray-300 dark:border-gray-700/70 checked:before:absolute checked:before:bg-white checked:before:inset-1 checked:before:rounded-full checked:bg-blue-600 checked:border-blue-600 disabled:bg-black/5 disabled:dark:bg-white/5 focus:outline-2 focus:outline-blue-600 focus:outline-offset-2 forced-colors:appearance-auto forced-colors:before:hidden relative rounded-full size-4';
+  const className = 'appearance-none border-1 border-gray-300 dark:border-gray-700/70 checked:before:absolute checked:before:bg-white checked:before:inset-1 checked:before:rounded-full checked:bg-blue-600 checked:border-blue-600 disabled:bg-black/5 disabled:dark:bg-white/5 focus-visible:outline-2 focus-visible:outline-blue-600 outline-offset-2 forced-colors:appearance-auto forced-colors:before:hidden relative rounded-full size-4';
   return className;
 };
 
